45f2a44ae42972bca098358c37c7b31e25186bc7
Pongsiri Ngamboontham <p_ngamboontham@hotmail.com>
2020-11-15 02:43:15 +0700

    Login, Auth, edit Blog are Done!!

--- security_backend/urls.py
+++ security_backend/urls.py
@@ -18,6 +18,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogs/' , include('manage_Blogs.urls')),
-    path('', include('authentication.urls'))
+    path('' , include('manage_Blogs.urls')),
 ]
