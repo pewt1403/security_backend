02264fc1702540825d805418c90c496bb58c01c0
Pongsiri Ngamboontham <p_ngamboontham@hotmail.com>
2020-11-14 16:59:42 +0700

    Playing with README.MD

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # secure_backend
+
+<p align="center">
+    <img src="./readme_src/model.png"></img>
+</p>
--- manage_Blogs/models.py
+++ manage_Blogs/models.py
@@ -3,11 +3,22 @@ from django.db import models
 # Create your models here.
 class Blogs(models.Model):
     title_text = models.CharField(max_length=100)
-    content_text = models.CharField()
+    content_text = models.CharField(max_length=2100)
     blogScore = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
 
+    @classmethod
+    def create(cls, title, content):
+        blog = cls(title_text=title, content_text=content, blogScore=0, )
+        return blog
+
 class Comment(models.Model):
     Blogs = models.ForeignKey(Blogs, on_delete=models.CASCADE)
-    comment_text = models.CharField()
+    creator = models.CharField(max_length=30)
+    pub_date =  models.DateTimeField('date published')
+    comment_text = models.CharField(max_length=700)
     commentScore = models.IntegerField(default=0)
+
+    @classmethod
+    def create(cls, comment):
+        return
