0437a87d3c9bdd3b9e0644fcdb57035e645396d6
Poompatai Puntitpong <poomtapai@hotmail.com>
2020-11-15 16:25:13 +0700

    Add homepage redirection

--- manage_Blogs/urls.py
+++ manage_Blogs/urls.py
@@ -1,17 +1,27 @@
 from django.urls import path
 
+from manage_Blogs.views import homePage
+
 from . import views
 
-app_name = 'manage_Blogs'
+app_name = "manage_Blogs"
 urlpatterns = [
+    # /
+    path("", views.homePage, name="home"),
     # /blogs/
-    path('blogs/', views.index, name='blogs'),
+    path("blogs/", views.index, name="blogs"),
     # /blogs/1
-    path('blogs/<int:blogId>/', views.blogDetail, name='blogDetail'),
-    path('blogs/create/', views.createBlog ,name='createBlog'),
-    path('blogs/createComment/<int:blogId>/', views.createComment, name='createComment'),
-    path('blogs/edit/<int:blogId>/', views.editBlog, name='editBlog'),
-    path('blogs/edit/<int:blogId>/<int:commentId>/', views.editComment, name='editComment'),
-    path('login/', views.loginPage, name='login'),
-    path('logout/', views.logoutPage, name='logout')
+    path("blogs/<int:blogId>/", views.blogDetail, name="blogDetail"),
+    path("blogs/create/", views.createBlog, name="createBlog"),
+    path(
+        "blogs/createComment/<int:blogId>/", views.createComment, name="createComment"
+    ),
+    path("blogs/edit/<int:blogId>/", views.editBlog, name="editBlog"),
+    path(
+        "blogs/edit/<int:blogId>/<int:commentId>/",
+        views.editComment,
+        name="editComment",
+    ),
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutPage, name="logout"),
 ]
